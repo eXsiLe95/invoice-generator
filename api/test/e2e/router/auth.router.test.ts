2a407cf427366862ee69ae7fc8784fcc95189b2b
Lukas Kimpel <lukas.kimpel@deutschebahn.com>
2024-06-02 19:12:27 +0200

    test: add test for login

--- api/test/e2e/router/auth.router.test.ts
+++ api/test/e2e/router/auth.router.test.ts
@@ -1,5 +1,6 @@
 import {afterEach, beforeEach, describe, expect, test} from 'vitest';
 import database from "../../../src/database";
+import bcrypt from "bcrypt";
 
 describe('API Test /auth', () => {
 
@@ -67,13 +68,12 @@ describe('API Test /auth', () => {
 
 	test('/register: fails to create a new user when email already in use', async () => {
 		// Given
-		await database.user.create({
+		const user = await database.user.create({
 			data: {
 				email,
-				password
+				password: await bcrypt.hash(password, 10)
 			}
 		});
-		const user = await database.user.findFirst();
 		expect(user).toBeDefined();
 		expect(user.email).toEqual(email);
 
@@ -90,6 +90,100 @@ describe('API Test /auth', () => {
 		expect(response.status).toEqual(400);
 		const responseBody = await response.json();
 		expect(responseBody.message).toEqual("email address already in use");
+	});
+
+	test('/login: logs in existing user', async () => {
+		// Given
+		const user = await database.user.create({
+			data: {
+				email,
+				password: await bcrypt.hash(password, 10)
+			}
+		});
+		expect(user).toBeDefined();
+		expect(user.email).toEqual(email);
+
+		// When
+		const response = await fetch('http://localhost:3000/auth/login', {
+			method: 'POST',
+			headers: {
+				'content-type': 'application/json',
+			},
+			body: JSON.stringify({email, password})
+		});
+
+		// Then
+		expect(response.status).toEqual(200);
+	});
+
+	test('/login: fails to log in non-existing user', async () => {
+		// Given
+
+		// When
+		const response = await fetch('http://localhost:3000/auth/login', {
+			method: 'POST',
+			headers: {
+				'content-type': 'application/json',
+			},
+			body: JSON.stringify({email, password})
+		});
+
+		// Then
+		expect(response.status).toEqual(401);
+		const responseBody = await response.json();
+		expect(responseBody.message).toEqual("email and password do not match");
+	});
+
+	test('/login: fails to log in user with incorrect password', async () => {
+		// Given
+		const user = await database.user.create({
+			data: {
+				email,
+				password: await bcrypt.hash(password, 10)
+			}
+		});
+		expect(user).toBeDefined();
+		expect(user.email).toEqual(email);
+
+		// When
+		const response = await fetch('http://localhost:3000/auth/login', {
+			method: 'POST',
+			headers: {
+				'content-type': 'application/json',
+			},
+			body: JSON.stringify({email, password: "some-wrong-password"})
+		});
+
+		// Then
+		expect(response.status).toEqual(401);
+		const responseBody = await response.json();
+		expect(responseBody.message).toEqual("email and password do not match");
+	});
+
+	test('/login: fails to log in user with incorrect email', async () => {
+		// Given
+		const user = await database.user.create({
+			data: {
+				email,
+				password: await bcrypt.hash(password, 10)
+			}
+		});
+		expect(user).toBeDefined();
+		expect(user.email).toEqual(email);
+
+		// When
+		const response = await fetch('http://localhost:3000/auth/login', {
+			method: 'POST',
+			headers: {
+				'content-type': 'application/json',
+			},
+			body: JSON.stringify({email: "some-wrong-email@localhost", password})
+		});
+
+		// Then
+		expect(response.status).toEqual(401);
+		const responseBody = await response.json();
+		expect(responseBody.message).toEqual("email and password do not match");
 	});
 
 	afterEach(async () => {
