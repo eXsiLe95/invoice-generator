d15e678565705ef6322bba38a6704d34e816a20a
Lukas Kimpel <lukas.kimpel@deutschebahn.com>
2024-06-03 09:26:15 +0200

    build: apply code style rules

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,13 +1,13 @@
-import express, { Express, Request, Response } from "express";
-import dotenv from "dotenv";
+import express, { Express, Request, Response } from 'express';
+import dotenv from 'dotenv';
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
 
-app.get("/", (req: Request, res: Response) => {
-	res.send("Hello world");
+app.get('/', (req: Request, res: Response) => {
+	res.send('Hello world');
 });
 
 app.listen(port, () => {
