f55028542a0c63226565245e38b814fadebdc67b
Lukas Kimpel <lukas.kimpel@deutschebahn.com>
2024-06-09 12:52:40 +0200

    feat: add supertest in order to run tests without a dedicated backend task

--- api/test/index.test.ts
+++ api/test/index.test.ts
@@ -1,9 +1,12 @@
 import { describe, expect, test } from 'vitest';
+// eslint-disable-next-line @typescript-eslint/no-require-imports
+import request = require('supertest');
+import app from '../src/app';
 
 describe('API Test', () => {
 	test('server is running', async () => {
-		const response = await fetch('http://localhost:3000');
+		const response = await request(app).get('/').send();
 		expect(response.status).toEqual(200);
-		expect(await response.text()).toEqual('Hello world');
+		expect(response.text).toEqual('Hello world');
 	});
 });
