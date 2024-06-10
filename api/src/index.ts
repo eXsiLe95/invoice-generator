0dd6323ca31483dab13ca88c7f461c2a4ddb5c79
Lukas Kimpel <31202904+eXsiLe95@users.noreply.github.com>
2024-06-10 17:43:48 +0200

    test: add supertest in order to execute tests locally (#8)
    * feat: add supertest in order to run tests without a dedicated backend task
    
    * feat: enable tests in pipeline
    
    * fix: enable esModuleInterop for tests

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,15 +1,10 @@
-import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
+import app from './app';
 
 dotenv.config();
-
-const app: Express = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req: Request, res: Response) => {
-	res.send('Hello world');
-});
-
 app.listen(port, () => {
+	// eslint-disable-next-line no-console
 	console.log(`[server]: Server is running at http://localhost:${port}`);
 });
--- api/test/index.test.ts
+++ api/test/index.test.ts
@@ -1,9 +1,11 @@
 import { describe, expect, test } from 'vitest';
+import request from 'supertest';
+import app from '../src/app';
 
 describe('API Test', () => {
 	test('server is running', async () => {
-		const response = await fetch('http://localhost:3000');
+		const response = await request(app).get('/').send();
 		expect(response.status).toEqual(200);
-		expect(await response.text()).toEqual('Hello world');
+		expect(response.text).toEqual('Hello world');
 	});
 });
